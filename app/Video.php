8b4194567243518ebae95acc2b8cdf495b355db0
VladimirShchigolev <32202503+VladimirShchigolev@users.noreply.github.com>
2020-06-13 23:27:04 +0300

    Tables and Models
    Defined migrations, models
    created empty controllers

---
+++ app/Video.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    protected $fillable = ['owner', 'path', 'title', 'description', 'blocked'];
+    
+    public function owner() {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+    
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
+}
---
+++ database/migrations/2020_06_13_180727_create_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('owner');
+            $table->string('path', 100)->unique();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->boolean('blocked');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
